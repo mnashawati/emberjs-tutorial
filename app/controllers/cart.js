66d8a55dccd51806d99147f2bfd6304a40b05034
Mo Nashawati <mo.nashawati@centrica.com>
2021-02-24 17:01:22 +0000

    Connected cart controller to model to get values

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,7 +1,17 @@
 import Controller from '@ember/controller';
 
 export default class CartController extends Controller {
-    subtotal = 0;
-    tax = 0; 
-    total = 0;
+    get subtotal() {
+        return this.model.reduce((acc, item) => {
+            return acc + item.price;
+        }, 0);
+    }
+
+    get tax() {
+        return 0.10 * this.subtotal;
+    }
+
+    get total() {
+        return this.subtotal + this.tax;
+    }
 }
