ef68cb64904fcb7919b55ba1bfabd3a7de0f8a41
Mo Nashawati <mo.nashawati@centrica.com>
2021-02-24 12:03:54 +0000

    first commit

---
+++ app/router.js
@@ -0,0 +1,13 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'emberjs-tutorial/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function () {
+  this.route('clothes', function() {
+    this.route('t-shirt');
+  });
+});
