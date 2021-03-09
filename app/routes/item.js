4296e0540d5a0e4cf386515e37d84b3bf376c25a
Mo Nashawati <mo.nashawati@centrica.com>
2021-03-09 16:06:17 +0000

    Pulling product details from data object

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { products } from '../data/product';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
     model(params) {
