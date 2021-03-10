22e051c789fa943729181817844cdc4147524cbc
Mo Nashawati <mo.nashawati@centrica.com>
2021-03-10 11:11:13 +0000

    Defined a class helper "currency"

--- app/helpers/currency.js
+++ app/helpers/currency.js
@@ -1,5 +1,17 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 
-export default helper(function currency(params/*, hash*/) {
-  return params;
-});
+export default class currency extends Helper {
+  compute(params, hash = {}) {
+    // {{currency 25}}
+    const [number] = params;
+    const { sign = '£' } = hash;
+    const pounds = Math.floor(number);
+    let pence = Math.floor(number * 100 % 100);
+
+    if (pence.toString().length === 1) {
+      pence = '0' + pence;
+    }
+
+    return `${sign}${pounds}.${pence}`;
+  }
+};
