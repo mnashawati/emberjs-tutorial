1a3b6540be8171c8ba54b08a8ed78ddf8c068158
Mo Nashawati <mo.nashawati@centrica.com>
2021-03-10 11:12:59 +0000

    Added missing setupController

--- app/routes/item.js
+++ app/routes/item.js
@@ -5,7 +5,11 @@ export default class ItemRoute extends Route {
     model(params) {
         const { item_id } = params;
         const product = products.find(({ id }) => id === item_id);
-        console.log(product)
         return product;
+    }
+
+    setupController(controller, model) {
+        super.setupController(controller, model);
+        controller.color = model.colors[0].color;
     }
 }
